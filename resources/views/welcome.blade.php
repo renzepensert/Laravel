1aad0251d5f3198927ad577a2a359789518947cc
Rens <Rens_groot@hotmail.com>
2020-06-10 16:31:32 +0200

    added login screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Renzepensert</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+		
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
