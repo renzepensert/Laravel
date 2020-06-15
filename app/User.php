5a3a28a3cc8ef1e21eb4ee3b5d55db773bd91a28
Rens <Rens_groot@hotmail.com>
2020-06-15 19:23:42 +0200

    added controllers

--- app/User.php
+++ app/User.php
@@ -41,5 +41,10 @@ class User extends Authenticatable
 	{
 		return $this->hasOne(Profile::class);
 	}
+	
+	public function posts()
+	{
+		return $this->hasMany(Post::class);
+	}
 		
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,7 +7,10 @@
 			<img src="/svg/icon.svg" height="80px" class="image-rounded">
 		</div>
 		<div class="col-9 pt-5">
-			<div><h1>{{ $user-> username}}</h1></div>
+			<div class="d-flex justify-content-between align-items-baseline">
+				<h1>{{ $user-> username}}</h1>
+				<a href='#'>Add new post</a>
+			</div>
 			<div class="d-flex">
 				<div class="pr-5"><strong>153</strong> posts</div>
 				<div class="pr-5"><strong>23k</strong> followers</div>
